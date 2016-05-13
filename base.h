2ce1528288a01ebba2bedd9a22f9d8496e42c358
Guilherme Budnieswski <budnieswski@gmail.com>
2016-05-13 14:19:11 -0300

    Add basic pilha manipulation

--- base.h
+++ base.h
@@ -4,10 +4,82 @@
 #define BECO        0x2D // -
 #define ATUAL       0x40 // @
 
-typedef struct NODO
+typedef struct Nodo
 {
   int coordenadas;
-  struct NODO *prox;
-} NODO;
+  struct Nodo *prox;
+} Nodo;
 
 int labirinto[30][30];
+
+
+/**
+ * Inicializa a pilha como vazia
+ * @param N [Ponteiro da pilha]
+ */
+void inicializa (Nodo **N)
+{
+  *N = NULL;
+}
+
+
+/**
+ * Aloca espaco em memoria referente a pilha
+ * @return [Ponteiro de pilha]
+ */
+Nodo * criaNodo ()
+{
+  Nodo *n;
+  n = (Nodo *) malloc( sizeof(Nodo) );
+ 
+  if (!n)
+  {
+    printf("Erro durante a alocação\n");
+    exit(0);
+  }
+ 
+  return n;
+}
+
+
+/**
+ * Insere um elemento no inicio da pilha
+ * @param Pilha [Referencia do ponteiro da pilha]
+ * @param lin   [Linha da coordenada a ser armazenada]
+ * @param col   [Coluna da coordenada a ser armazenada]
+ */
+void insere (Nodo **Pilha, int lin, int col)
+{
+  Nodo *n;
+ 
+  n = criaNodo();
+  n->coordenadas = ((lin*100) + col); // OBS-1
+  n->prox = *Pilha;
+
+  /**
+   * [OBS-1]
+   * Armazena as coordenadas em uma unica variavel
+   * Facilita na busca e comparacao de elementos */
+ 
+  *Pilha = n;
+}
+
+
+/**
+ * Libera todo o espaco em memoria ocupado pela pilha
+ * @param Pilha [Referencia do ponteiro da pilha]
+ */
+void liberarEspaco (Nodo **Pilha)
+{
+  if (*Pilha != NULL)
+  {
+    Nodo *aux, *tmp  = *Pilha;
+    while (tmp != NULL)
+    {
+      aux = tmp;
+      tmp = tmp->prox;
+      free (aux);
+    }
+    *Pilha = NULL;
+  }
+}
