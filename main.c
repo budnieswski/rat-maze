533afca3cff726e49eb0a122e95af1b68230a6c8
Guilherme Budnieswski <budnieswski@gmail.com>
2016-05-12 22:21:33 -0300

    Change maze defines

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PAREDE 		0x2A // *
+#define PAREDE 		0x23 // #
 #define LIVRE 		0x20 // espaco em branco
 #define VISITADA 	0x2E // .
-#define BECO 		0x23 // #
+#define BECO 		0x2D // -
 #define ATUAL  		0x40 // @
 
 int labirinto[30][30];
